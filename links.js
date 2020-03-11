409be5e946fffa73c1d1a26db7a7b90ca1cd87d1
azul-de-prusia <tegthyn_600@hotmail.com>
2020-03-11 14:13:30 -0600

    poner texto y links en un objeto

--- links.js
+++ links.js
@@ -1,14 +1,15 @@
 const fetch = require('node-fetch');
 const { errL } = require('./message.js');
 const objLinks = (linksArray) => {
+    let obj = {}
     const regexText = /\[(.*?)\]/gm;
     const regexLinks = /\((.*?)\)/gm;
     for (let i = 0; i < linksArray.length; i++) {
-        const obj = {
-            links: linksArray[i].match(regexLinks),
-            text: linksArray[i].match(regexText),
+        obj = {
+            links: linksArray[i].match(regexLinks)[0].replace('(', '').replace(')', ''),
+            text: linksArray[i].match(regexText)[0].replace('[', '').replace(']', ''),
         }
-        console.log(obj)
+        responseHttp(obj)
     }
 }
 const counters = (links, ) => {
@@ -17,20 +18,22 @@ const counters = (links, ) => {
     let unique = 0;
 
 }
-const responseHttp = (links) => {
+const responseHttp = (obj) => {
     //petición por medio de fetch
-    for (let i = 0; i < links.length; i++) {
-        fetch(links[i])
-            .then(res => {
-                res.text();
-                //para contar
-                okCounter++
-            })
-            .then(links => {
-                //para contar
-                failCounter++
-            });
-    }
+    obj.forEach(element => {
+
+    });
+    fetch(links[i])
+        .then(res => {
+            res.text();
+            //para contar
+            okCounter++
+        })
+        .then(links => {
+            //para contar
+            failCounter++
+        });
+}
 }
 const links = (content) => {
     const regexLinksAndText = /\[(.*?)\]\((.*?)\)/gm;
